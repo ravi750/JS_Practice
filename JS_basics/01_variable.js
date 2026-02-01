903bfd5890315015b49a94956e17772e54629409
ravi750 <rs.csm15@gmail.com>
2026-02-01 19:49:48 +0530

    JS_basics

---
+++ JS_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 133442;
+let accountEmail = 'ravi@apple.com';
+var accountPassword = '12345';
+var accountCity = 'Hyderabad';
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
